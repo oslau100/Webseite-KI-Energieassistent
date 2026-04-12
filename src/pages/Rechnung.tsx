5a3fbe5c496688bd3bb387730128f7c7f6438ea8
Eddy <oslau100@gmail.com>
2026-04-12 13:45:43 +0200

    Refine master status copy and improve localized error layouts

---
+++ src/pages/Rechnung.tsx
@@ -0,0 +1,10 @@
+import { SimpleFooter } from "@/components/SimpleFooter";
+
+const Rechnung = () => (
+  <div className="min-h-screen flex flex-col bg-background text-foreground font-sans selection:bg-primary/20 selection:text-primary">
+    <main className="flex-grow p-2 sm:p-4" />
+    <SimpleFooter />
+  </div>
+);
+
+export default Rechnung;
